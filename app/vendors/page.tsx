10a6e56b6b56b50e717c3e4f73b1255f1070e02e
pilot1959 <mdkeith1@gmail.com>
2026-02-10 16:22:37 -0800

    Update page.tsx

--- app/vendors/page.tsx
+++ app/vendors/page.tsx
@@ -1,12 +1,17 @@
-export default function VendorDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+import Link from 'next/link';
+
+export default function VendorsPage() {
   return (
     <div style={{ padding: 24 }}>
-      <h1 style={{ fontSize: 24, fontWeight: 600 }}>Vendor Detail</h1>
-      <p>Vendor ID: {params.id}</p>
+      <h1 style={{ fontSize: 24, fontWeight: 600 }}>Vendors</h1>
+
+      <ul style={{ marginTop: 16 }}>
+        <li>
+          <Link href="/vendors/test-vendor">
+            Test Vendor
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
