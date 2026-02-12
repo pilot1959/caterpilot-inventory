ad602dc5beec26698335443add91d5c64c09dcf0
pilot1959 <mdkeith1@gmail.com>
2026-02-12 13:43:01 -0800

    Update page.tsx

--- app/vendors/[id]/page.tsx
+++ app/vendors/[id]/page.tsx
@@ -1,10 +1,5 @@
 import VendorDetailClient from "@/app/components/vendors/VendorDetailClient";
 
-export default function VendorDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  // App Router MUST pass params.id here when URL is /vendors/<id>
-  return <VendorDetailClient id={params.id} />;
+export default function VendorDetailPage() {
+  return <VendorDetailClient />;
 }
