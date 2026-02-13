1d88bdd13184b5512d3f4618e8e52de7696b46b4
pilot1959 <mdkeith1@gmail.com>
2026-02-12 20:31:55 -0800

    Update page.tsx

--- app/vendors/[id]/page.tsx
+++ app/vendors/[id]/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useParams } from "next/navigation";
 import VendorDetailClient from "@/app/components/vendors/VendorDetailClient";
 
 export default function VendorDetailPage() {
-  return <VendorDetailClient />;
+  const params = useParams();
+
+  // Next can return string | string[] | undefined
+  const id =
+    typeof (params as any)?.id === "string"
+      ? ((params as any).id as string)
+      : Array.isArray((params as any)?.id)
+      ? ((params as any).id[0] as string)
+      : "";
+
+  return <VendorDetailClient id={id} />;
 }
