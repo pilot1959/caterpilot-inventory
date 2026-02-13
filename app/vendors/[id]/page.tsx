3b38b309130983fb5995133891630ce8e2327e33
pilot1959 <mdkeith1@gmail.com>
2026-02-12 19:44:13 -0800

    Update page.tsx

--- app/vendors/[id]/page.tsx
+++ app/vendors/[id]/page.tsx
@@ -1,5 +1,12 @@
 import VendorDetailClient from "@/app/components/vendors/VendorDetailClient";
 
-export default function VendorDetailPage({ params }: { params: { id: string } }) {
+export default function VendorDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  // IMPORTANT:
+  // This file must NOT have "use client"
+  // It should stay a server component so params always works.
   return <VendorDetailClient id={params.id} />;
 }
